c1b4c201425f40364395ba8835fbe3b97ffb9d81
pat <pat@localhost.localdomain>
2017-06-10 23:00:18 -0400

    add sms status to saved address displays
    fix #4 - adds sms enabled flag to account addresses listed in customer address book.
    instructions for manually adding to html address template were added to readme.
    
    sms enabled flag was also added to the saved addresses during checkout.

--- view/frontend/web/js/action/place-order-mixin.js
+++ view/frontend/web/js/action/place-order-mixin.js
@@ -24,11 +24,32 @@ define([
   return function (placeOrderAction) {
     return wrapper.wrap(placeOrderAction, function(originalAction) {
       var billingAddress = quote.billingAddress();
+
+      if(billingAddress.customAttributes === undefined) {
+        billingAddress.customAttributes = {};
+      }
+
       if(billingAddress['extension_attributes'] === undefined) {
         billingAddress['extension_attributes'] = {};
       }
 
-      billingAddress['extension_attributes']['sms_alert'] = billingAddress.customAttributes['sms_alert'] ? 1 : 0;
+      try {
+        var smsStatus = billingAddress.customAttributes['sms_alert'].status;
+        var smsValue = billingAddress.customAttributes['sms_alert'].value;
+
+        //TODO: this smells. the check for status stems from an attempt to
+        // non-invasively display a user friendly custom attribute value in
+        // the customer address-mixin.
+        if(smsStatus == true) {
+          billingAddress['extension_attributes']['sms_alert'] = smsStatus;
+        }else if(smsValue == true) {
+          billingAddress['extension_attributes']['sms_alert'] = smsValue
+        }else {
+          billingAddress['extension_attributes']['sms_alert'] = false;
+        }
+      }catch (e) {
+        return originalAction();
+      }
 
       return originalAction();
     });
--- view/frontend/web/js/action/set-shipping-information-mixin.js
+++ view/frontend/web/js/action/set-shipping-information-mixin.js
@@ -33,7 +33,23 @@ define([
         shippingAddress['extension_attributes'] = {};
       }
 
-      shippingAddress['extension_attributes']['sms_alert'] = shippingAddress.customAttributes['sms_alert'] ? 1 : 0;
+      try {
+        var smsStatus = shippingAddress.customAttributes['sms_alert'].status;
+        var smsValue = shippingAddress.customAttributes['sms_alert'].value;
+
+        //TODO: this smells. the check for status stems from an attempt to
+        // non-invasively display a user friendly custom attribute value in
+        // the customer address-mixin.
+        if(smsStatus == true) {
+          shippingAddress['extension_attributes']['sms_alert'] = smsStatus;
+        }else if(smsValue == true) {
+          shippingAddress['extension_attributes']['sms_alert'] = smsValue
+        }else {
+          shippingAddress['extension_attributes']['sms_alert'] = false;
+        }
+      }catch (e) {
+        return originalAction();
+      }
 
       return originalAction();
     });
